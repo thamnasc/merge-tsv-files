7f97c6667e533128569a910a874d03172b33613a
thamnasc <thalitanas09@gmail.com>
2022-08-16 21:17:07 -0300

    get the header and comments

--- mergeTSV.sh
+++ mergeTSV.sh
@@ -2,34 +2,34 @@
 
 function get_header_and_urls
 {
-    cat $1 | head -1 >> gbn-fonts.tsv
-    COUNTER_LINES=$(1)
-    #STR='# '
-    #for ROW in $(cat $1)
-    #do
-        #if [[ $STR =~ "$ROW" ]]; then
-       #     COUNTER_LINES=$((COUNTER_LINES+1))
-      #      ROW >> gbn-fonts.tsv
-     #   fi
-    #done
+    declare -i COUNTER_LINES=1
 
-    local COUNTER=$(cat $1 | awk -v counter=$COUNTER_LINES -F "\t" '{
-        for (i=1; i<=NF; i++)
-        {
-            if(index($1, "# ") != 0)
-            {
-                
-                counter++;
-            }
-        }
-        print counter;
-    }')
+    while IFS=$'\t' read -ra row
+    do
+        if [[ "# " == *"${row[1]}"* ]]; then
+            COUNTER_LINES=$((COUNTER_LINES+1))
+        fi
+        
+    done < "$1"
+
+    cat $1 | head -$COUNTER_LINES > gbn-fonts.tsv
 
     echo $COUNTER_LINES
 }
 
-HEADER=$(get_header_and_urls $1)
-echo $HEADER linhas
-#print $1 >> "gbn-fonts.tsv";
-#FILES=("gbn-fonts-1.tsv", "gbn-fonts-2.tsv", "gbn-fonts-3.tsv")
-#print $1 >> "gbn-fonts.tsv";
+function build_tsv_file 
+{
+    FILES=("gbn-fonts-1.tsv", "gbn-fonts-2-fim.tsv", "gbn-fonts-3_part_0(final).tsv",
+    "gbn-fonts-3_part_1", "gbn-fonts-3_part_1_(2)", "gbn-fonts-3_part_2_done", 
+    "gbn-fonts-3_part_3.txt", "gbn-fonts-3_part_4_(final).tsv", "gbn-fonts-4_FInalOficial.tsv")
+    
+    FILE=("gbn-fonts-1.tsv")
+    for file in ${FILE[@]}; do
+        echo $file | tail -n +$1 >> gbn-fonts.tsv
+    done
+}
+
+LINES=$(get_header_and_urls $1)
+build_tsv_file $LINES
+echo $LINES linhas
+
