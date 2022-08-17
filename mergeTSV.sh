2f442e13ee84474fb5b9c1207f5374a3bf61bb95
thamnasc <thalitanas09@gmail.com>
2022-08-17 20:20:50 -0300

    the last comment is duplicated

--- mergeTSV.sh
+++ mergeTSV.sh
@@ -19,14 +19,12 @@ function get_header_and_urls
 
 function build_tsv_file 
 {
-    FILES=('gbn-fonts-1.tsv', 'gbn-fonts-2-fim.tsv', 'gbn-fonts-3_part_0(final).tsv', 'gbn-fonts-3_part_1', 'gbn-fonts-3_part_1_(2)', 'gbn-fonts-3_part_2_done', 'gbn-fonts-3_part_3.txt', 'gbn-fonts-3_part_4_(final).tsv', 'gbn-fonts-4_FInalOficial.tsv')
+    FILES=('gbn-fonts-0.tsv' 'gbn-fonts-1.tsv' 'gbn-fonts-2-fim.tsv' 'gbn-fonts-3_part_0(final).tsv' 'gbn-fonts-3_part_1_(2)' 'gbn-fonts-3_part_2_done' 'gbn-fonts-3_part_3.txt' 'gbn-fonts-3_part_4_(final).tsv' 'gbn-fonts-4_FInalOficial.tsv')
     for file in "${FILES[@]}"; do
         cat $file | tail -n +$1 >> gbn-fonts.tsv
     done
 }
 
 LINES=$(get_header_and_urls $1)
-echo $LINES linhas
 build_tsv_file $LINES
-
 
