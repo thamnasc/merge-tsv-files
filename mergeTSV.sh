3ef72d77227319d9fa0219414186f304e95be562
thamnasc <thalitanas09@gmail.com>
2022-08-17 20:01:23 -0300

    wip

--- mergeTSV.sh
+++ mergeTSV.sh
@@ -19,17 +19,14 @@ function get_header_and_urls
 
 function build_tsv_file 
 {
-    FILES=("gbn-fonts-1.tsv", "gbn-fonts-2-fim.tsv", "gbn-fonts-3_part_0(final).tsv",
-    "gbn-fonts-3_part_1", "gbn-fonts-3_part_1_(2)", "gbn-fonts-3_part_2_done", 
-    "gbn-fonts-3_part_3.txt", "gbn-fonts-3_part_4_(final).tsv", "gbn-fonts-4_FInalOficial.tsv")
-    
-    FILE=("gbn-fonts-1.tsv")
-    for file in ${FILE[@]}; do
-        echo $file | tail -n +$1 >> gbn-fonts.tsv
+    FILES=('gbn-fonts-1.tsv', 'gbn-fonts-2-fim.tsv', 'gbn-fonts-3_part_0(final).tsv', 'gbn-fonts-3_part_1', 'gbn-fonts-3_part_1_(2)', 'gbn-fonts-3_part_2_done', 'gbn-fonts-3_part_3.txt', 'gbn-fonts-3_part_4_(final).tsv', 'gbn-fonts-4_FInalOficial.tsv')
+    for file in "${FILES[@]}"; do
+        cat $file | tail -n +$1 >> gbn-fonts.tsv
     done
 }
 
 LINES=$(get_header_and_urls $1)
-build_tsv_file $LINES
 echo $LINES linhas
+build_tsv_file $LINES
+
 
